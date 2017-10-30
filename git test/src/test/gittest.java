56dbda7c80fcade6dc8217dd3caa71f26017d02a
ifsili <33216509+ifsili@users.noreply.github.com>
2017-10-30 17:26:31 +0900

    Update gittest.java

--- git test/src/test/gittest.java
+++ git test/src/test/gittest.java
@@ -5,6 +5,9 @@ public class gittest {
 		System.out.println("test git");
 		System.out.println("추가작업을 해봐영");
 		System.out.println("totototo");
+			System.out.println("에러내보장 해봐영");
+				System.out.println("추가작업을 해봐영");
+				System.out.println("추가작업을 해봐영");
 	}
 
 }
